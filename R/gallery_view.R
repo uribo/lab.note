92678f5ea8c6d8f0297ee288d8f2ca7c3ec3615e
Shinya Uryu <suika1127@gmail.com>
2015-05-13 08:36:07 +0900

    [FIX]follow CRAN policy

--- R/gallery_view.R
+++ R/gallery_view.R
@@ -1,6 +1,9 @@
 #' listup image files
+#' 
+#' @usage gallery_view(path = getwd())
+#' @param path location of the figure include folder
 #' @export
-gallery_view <- function (path = getwd()) {
+gallery_view <- function(path = getwd()) {
   files <- list.files(path, recursive = TRUE, full.names = TRUE) 
   files <- grep(pattern = ".gif$|.jpg$|.png$", files, value = TRUE)
   for (i in 1:length(files)) {
---
+++ R/lab.note.R
@@ -0,0 +1,7 @@
+##' lab.note: Create Laboratory Note and Presentation Using R Markdown
+##'
+##' @docType package
+##' @name lab.note
+##' @import knitr
+##' @import rmarkdown
+NULL
